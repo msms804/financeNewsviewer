da407de10861b49b72d0378db44a1dea9d9366c1
msms804 <104570214+msms804@users.noreply.github.com>
2024-08-04 00:54:12 +0900

    .env에 api키 보관 수정

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBwkSxUNYzlincH9RD-QSsB9GqdZNEqWF4",
-    authDomain: "finance-newsviewer.firebaseapp.com",
-    projectId: "finance-newsviewer",
-    storageBucket: "finance-newsviewer.appspot.com",
-    messagingSenderId: "554209465900",
-    appId: "1:554209465900:web:552e9e6451c79ed70a2389"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 
