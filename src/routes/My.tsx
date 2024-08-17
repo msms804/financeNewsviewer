8109cabfa6fa62bd36f4d210d31e4fee99da37a7
msms804 <104570214+msms804@users.noreply.github.com>
2024-08-17 23:51:12 +0900

    내 관심종목에 해당하는 뉴스 가져와 렌더링

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -4,12 +4,17 @@ import { auth } from '../firebase';
 import { collection, getDocs, limit, orderBy, query, where } from 'firebase/firestore';
 import { db } from '../firebase';
 import { IStock } from './Edit';
+import axios from 'axios';
+import { INews } from '../components/NewsList';
+import { Favicon } from '../components/Favicon';
 
 export const My = () => {
     const navigate = useNavigate();
     const user = auth.currentUser;
     const [avatar, setAvatar] = useState(user?.photoURL);
     const [myStocks, setMyStocks] = useState<IStock[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [news, setNews] = useState<INews[] | null>(null);
 
     //1. db에서 fetch 한다. 여기서는 실시간 할 필요 없을듯
     //2. 일단 렌더링한다.
@@ -40,6 +45,56 @@ export const My = () => {
         fetchMyStocks();
     }, [])
 
+
+    useEffect(() => {//기사를 최근 3일치만 가져오면 될듯
+        const fetchNews = async () => {
+            if (!myStocks) return;
+            setLoading(true);
+            const apiKey = import.meta.env.VITE_NYTIMES_API_KEY;
+            // 이거 테마를 비즈니스로 한정할 수 있나?
+            //hint : join
+            const newMyStocks = myStocks.map(stock => {
+                return stock.englishName.split(' ')[0].replace(',', '')
+            }).join(',');
+            console.log(">>newMyStocks", newMyStocks);
+
+            const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=NVIDIA,Microsoft&api-key=${apiKey}`;
+
+            try {
+                const response = await axios.get(url);
+
+                // relatedStock, article을 새로운 배열로 만들기
+                const newsWithStocks = response.data.response.docs.map((article: any) => {
+                    //관련주식 찾기 
+                    //hint: find, some, includes
+                    const relatedStock = myStocks.find(stock =>
+                        article.keywords.some((keyword: any) =>
+                            (keyword.name == "subject" || keyword.name == "organizations") &&
+                            (keyword.value.includes(stock.englishName.split(' ')[0].replace(',', '')) || keyword.value.includes(stock.symbol))
+                            //이거걍 db에 변환해서 넣어야겠음 ;;
+                        )
+
+                    )
+                    return {
+                        ...article,
+                        relatedStock: relatedStock?.symbol,
+                    }
+                })
+                console.log(">> 확인", newsWithStocks)
+                //setNews(response.data.response.docs);
+                setNews(newsWithStocks);
+                console.log(">> 테슬라뉴스, 애플뉴스 : ", response.data)
+            } catch (error) {
+                console.error(error);
+            }
+            setLoading(false);
+        }
+        fetchNews();
+    }, [myStocks])
+
+    //gpt로 변환
+    if (loading) return <div>뉴스 로딩중...</div>
+
     return (
         <div className=''>
             {/* <div onClick={() => navigate('/login')}>
@@ -47,15 +102,6 @@ export const My = () => {
             </div> */}
             <div className='flex flex-row justify-between items-center border-b border-gray-200 m-2 p-2'>
                 <div className='flex flex-row items-center justify-center'>
-                    {/* {Boolean(avatar) ? <img src={avatar} /> : <svg
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                        aria-hidden="true"
-                        className='w-8 h-8'
-                    >
-                        <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
-                    </svg>} */}
                     <span className='text-lg font-semibold'>{user?.displayName ?? "Anonymous"} 님이 찜한 주식뉴스</span>
                 </div>
 
@@ -67,12 +113,22 @@ export const My = () => {
                         </svg>
                         <h1 className='text-sm font-semibold text-gray-500'>편집</h1>
                     </div>
-                    {/* <div>나머지는 1. 토스, 2. 뉴스탭 3. ~님이 찜한 주식뉴스</div> */}
+
                 </div>
             </div>
+            {/* <div className='flex flex-col gap-1'>
+                {myStocks && myStocks.map((stock) =>
+                    <div key={stock.id}
+                        className='flex flex-row items-center gap-1 text-xs text-gray-500 border border-gray-500 py-1 px-2 rounded-full'>
+                        <img src='https://logo.clearbit.com/tesla.com' className='w-3 h-3 rounded-full' />
+                        <div className=''>
+                            {stock.symbol}
+                        </div>
+                    </div>)}
+            </div> */}
 
             {/* 종목별 뉴스 */}
-            <div className='flex flex-col gap-1 mb-4'>
+            {/* <div className='flex flex-col gap-1 mb-4'>
                 <div>
                     <span className='text-xs text-red-600 font-bold '>테슬라 1.2%</span>
                 </div>
@@ -81,17 +137,20 @@ export const My = () => {
                     <span>연합인포맥스</span>
                     <span> - 1시간 전</span>
                 </div>
-            </div>
-            <div className='flex flex-col gap-1 mb-4'>
-                <div>
-                    <span className='text-xs text-blue-500 font-bold '>쿠팡 1.2%</span>
-                </div>
-                <span>이커머스 기업 쿠팡, 2분기 순손실 전환</span>
-                <div className='text-xs text-gray-500'>
-                    <span>로이터</span>
-                    <span> - 3시간 전</span>
-                </div>
-            </div>
+            </div> */}
+
+            {
+                news && news.map((article) => <div key={article._id} className='flex flex-col gap-1 mb-4'>
+                    <div>
+                        <span className='text-xs text-blue-500 font-bold '>{article?.relatedStock}</span>
+                    </div>
+                    <span className='font-thin text-sm'>{article.abstract}</span>
+                    <div className='text-xs text-gray-500'>
+                        <span>{article.source}</span>
+                        <span> - 3시간 전</span>
+                    </div>
+                </div>)
+            }
 
         </div>
     )
