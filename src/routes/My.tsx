83fd1933fe013c789b5dc2ad6f251576625b60b2
msms804 <104570214+msms804@users.noreply.github.com>
2024-08-18 00:57:41 +0900

    해당뉴스의 관련종목들도 나타내는 기능구현

--- src/routes/My.tsx
+++ src/routes/My.tsx
@@ -58,30 +58,27 @@ export const My = () => {
             }).join(',');
             console.log(">>newMyStocks", newMyStocks);
 
-            const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=NVIDIA,Microsoft&api-key=${apiKey}`;
+            const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=NVIDIA,Microsoft,apple&api-key=${apiKey}`;
 
             try {
                 const response = await axios.get(url);
 
                 // relatedStock, article을 새로운 배열로 만들기
                 const newsWithStocks = response.data.response.docs.map((article: any) => {
-                    //관련주식 찾기 
-                    //hint: find, some, includes
-                    const relatedStock = myStocks.find(stock =>
-                        article.keywords.some((keyword: any) =>
-                            (keyword.name == "subject" || keyword.name == "organizations") &&
-                            (keyword.value.includes(stock.englishName.split(' ')[0].replace(',', '')) || keyword.value.includes(stock.symbol))
-                            //이거걍 db에 변환해서 넣어야겠음 ;;
-                        )
 
-                    )
+                    const relatedStocks = myStocks.filter((stock: any) => {
+                        return article.keywords.some((keyword: any) =>
+                            (keyword.name === "organizations" || keyword.name === "subject") &&
+                            keyword.value.includes(stock.englishName.split(' ')[0].replace(',', ''))
+                        )
+                    })
+                    console.log("관련주식 ", relatedStocks);
                     return {
                         ...article,
-                        relatedStock: relatedStock?.symbol,
+                        relatedStock: relatedStocks.map(stock => stock.symbol),
                     }
                 })
                 console.log(">> 확인", newsWithStocks)
-                //setNews(response.data.response.docs);
                 setNews(newsWithStocks);
                 console.log(">> 테슬라뉴스, 애플뉴스 : ", response.data)
             } catch (error) {
@@ -97,9 +94,7 @@ export const My = () => {
 
     return (
         <div className=''>
-            {/* <div onClick={() => navigate('/login')}>
-                로그인하세요 굳이? 어차피 로그인안하면 이화면 안뜸 / 사용자 정보로 바꾸셈
-            </div> */}
+
             <div className='flex flex-row justify-between items-center border-b border-gray-200 m-2 p-2'>
                 <div className='flex flex-row items-center justify-center'>
                     <span className='text-lg font-semibold'>{user?.displayName ?? "Anonymous"} 님이 찜한 주식뉴스</span>
@@ -116,33 +111,11 @@ export const My = () => {
 
                 </div>
             </div>
-            {/* <div className='flex flex-col gap-1'>
-                {myStocks && myStocks.map((stock) =>
-                    <div key={stock.id}
-                        className='flex flex-row items-center gap-1 text-xs text-gray-500 border border-gray-500 py-1 px-2 rounded-full'>
-                        <img src='https://logo.clearbit.com/tesla.com' className='w-3 h-3 rounded-full' />
-                        <div className=''>
-                            {stock.symbol}
-                        </div>
-                    </div>)}
-            </div> */}
-
-            {/* 종목별 뉴스 */}
-            {/* <div className='flex flex-col gap-1 mb-4'>
-                <div>
-                    <span className='text-xs text-red-600 font-bold '>테슬라 1.2%</span>
-                </div>
-                <span>테슬라, 태국 전기차 공장 설립 계획 철회</span>
-                <div className='text-xs text-gray-500'>
-                    <span>연합인포맥스</span>
-                    <span> - 1시간 전</span>
-                </div>
-            </div> */}
 
             {
                 news && news.map((article) => <div key={article._id} className='flex flex-col gap-1 mb-4'>
                     <div>
-                        <span className='text-xs text-blue-500 font-bold '>{article?.relatedStock}</span>
+                        <span className='flex flex-row text-xs text-blue-500 font-bold gap-1'>{article.relatedStock.map((stock: string) => <div key={stock} className='text-xs'>{stock}</div>)}</span>
                     </div>
                     <span className='font-thin text-sm'>{article.abstract}</span>
                     <div className='text-xs text-gray-500'>
