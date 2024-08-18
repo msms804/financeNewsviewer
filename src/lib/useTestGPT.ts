c650a0751af445a28e1b627c8882848d693c4c22
msms804 <104570214+msms804@users.noreply.github.com>
2024-08-18 23:54:03 +0900

    기사 호재인지 악재인지 판별해주는 gpt 활용 함수 작성

---
+++ src/lib/useTestGPT.ts
@@ -0,0 +1,29 @@
+export const useTestGPT = async (article: string, relatedStocks: string[]) => {
+    console.log(">>Call GPT");
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${import.meta.env.VITE_GPT_API_KEY}`,
+        },
+        body: JSON.stringify({
+            model: "gpt-4o-mini",
+            messages: [{
+                "role": "user",
+                "content": `다음 뉴스 기사를 분석하고, 이 기사가 주식 시장에 미치는 영향을 고려하여 전반적으로 호재인지 악재인지 판단해 주세요. 
+                호재로 판단되면 '호재', 악재로 판단되면 '악재'라고 간단히 답변해 주세요. 
+                뉴스 기사: ${article} 
+                관련 주식: ${relatedStocks}`
+            }],
+            temperature: 0.7,
+            max_tokens: 1000,
+        })
+
+    })
+
+    const responseData = await response.json();
+    const message = responseData.choices[0].message.content;
+
+    return message;
+}
