5abe848e56aeb9f01718a8ab5227bffdc59b01a2
msms804 <104570214+msms804@users.noreply.github.com>
2024-08-18 23:56:56 +0900

    기사제목을 보여주는 것으로 바꾸고 useTestGPT적용, 상세페이지로 navigate 추가

--- src/components/MyNewsItem.tsx
+++ src/components/MyNewsItem.tsx
@@ -1,25 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { INews } from './NewsList'
 import { useCallGPT } from '../lib/useCallGPT';
+import { useTestGPT } from '../lib/useTestGPT';
+import { useNavigate } from 'react-router-dom';
 
-export const MyNewsItem = ({ abstract, source, relatedStock }: INews) => {
+export const MyNewsItem = ({ abstract, source, relatedStock, _id, headline }: INews) => {
     const [newAbstract, setNewAbstract] = useState('');
+    const [isGood, setIsGood] = useState('');
+    const navigate = useNavigate();
 
+
+    const handleClick = () => {
+        const encodedId = encodeURIComponent(_id);
+        console.log("", encodedId);
+        navigate(`/mypage/news/${encodedId}`)
+    }
     useEffect(() => {
-        if (!abstract) return;
+        if (!abstract || !relatedStock) return;
         const convertToKorean = async () => {
-            const gptAbstract = await useCallGPT(abstract);
+            const gptAbstract = await useCallGPT(headline.main);
             setNewAbstract(gptAbstract)
         }
-        convertToKorean();
-    }, [abstract])
+        const analyzeNews = async () => {
+            const analyzed = await useTestGPT(abstract, relatedStock);
+            setIsGood(analyzed)
+        }
+        // convertToKorean();
+        // analyzeNews();
+
+    }, [abstract, relatedStock, headline])
     return (
         <div>
-            <div className='flex flex-col gap-1 mb-6'>
-                <div>
-                    <span className='flex flex-row text-xs text-blue-500 font-bold gap-1'>{relatedStock.map((stock: string) => <div key={stock} className='text-xs'>{stock}</div>)}</span>
+            <div onClick={handleClick} className='flex flex-col gap-1 mb-6 cursor-pointer'>
+                <div className='flex flex-row items-center gap-1'>
+                    {/* <span className={`text-xs p-1 rounded-md 
+                        ${isGood === "호재" ? 'bg-red-100 text-red-600' : 'bg-blue-100 text-blue-500'
+                        } `}>{isGood}</span> */}
+
+                    <span className='flex flex-row text-xs text-gray-500 font-bold gap-1'>{relatedStock.map((stock: string) => <div key={stock} className='text-xs'>{stock}</div>)}</span>
                 </div>
-                <span className='font-thin text-sm'>{newAbstract}</span>
+                <span className='font-thin text-sm'>{headline.main}</span>
                 <div className='text-xs text-gray-500'>
                     <span>{source}</span>
                     <span> - 3시간 전</span>
