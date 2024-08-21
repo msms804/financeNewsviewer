afd18fc6300b031c30e1424a10c37a20a7aa5db8
msms804 <104570214+msms804@users.noreply.github.com>
2024-08-21 20:31:00 +0900

    api 호출 제한이 있어 staleTime, gcTime 적용

--- src/lib/useNasdaq.ts
+++ src/lib/useNasdaq.ts
@@ -3,55 +3,49 @@ import axios from 'axios';
 import { useState, useEffect } from 'react'
 
 export const useNasdaq = () => {
-    const [loading, setLoading] = useState(false);
-    const [data, setData] = useState<any>(null);
-    const [error, setError] = useState<Error | null>(null);
-    const queryClient = useQueryClient();   //access the client\
+    // const [loading, setLoading] = useState(false);
+    // const [data, setData] = useState<any>(null);
+    // const [error, setError] = useState<Error | null>(null);
+    const queryClient = useQueryClient();   //access the client
 
     const fetchStocks = async () => {
-        setLoading(true);
-        try {
-            //리액트 쿼리로 가져오기
-            const response = await axios.get(
-                `https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=${import.meta.env.VITE_ALPHAVANTAGE_API_KEY}`,
-                { responseType: 'text' },
-            )
-            const textData = response.data;
-            console.log(">> 가공전 데이터", textData);
 
-            const parsedData = textData
-                .split("\n")
-                .slice(1)
-                .filter((line: any) => line.trim() !== "")  // 빈 줄 필터링
-                .map((line: any) => {
-                    const columns = line.split(",");
-                    const symbol = columns[0];
-                    const name = columns[1];
-                    const exchange = columns[2];  // 수정된 열 참조
-                    return { symbol, name, exchange };
-                });
+        //리액트 쿼리로 가져오기
+        const response = await axios.get(
+            `https://www.alphavantage.co/query?function=LISTING_STATUS&apikey=${import.meta.env.VITE_ALPHAVANTAGE_API_KEY}`,
+            { responseType: 'text' },
+        )
+        console.log(">> ", response);
+        const textData = response.data || "";
+        console.log(">> 가공전 데이터", textData);
 
-            console.log(">> parsedData:", parsedData);
-            const nasdaqStocks = parsedData.filter(
-                (stock: any) => stock.exchange.trim() === "NASDAQ"
-            );
+        const parsedData = textData
+            .split("\n")
+            .slice(1)
+            .filter((line: any) => line.trim() !== "")  // 빈 줄 필터링
+            .map((line: any) => {
+                const columns = line.split(",");
+                const symbol = columns[0];
+                const name = columns[1];
+                const exchange = columns[2];  // 수정된 열 참조
+                return { symbol, name, exchange };
+            });
 
-            console.log(">> 나스닥 필터링 데이터:", nasdaqStocks);
-
-            setData(nasdaqStocks);
-
-        } catch (error) {
-            setError(error as Error);
-        }
-        setLoading(false);
+        console.log(">> parsedData:", parsedData);
+        const nasdaqStocks = parsedData.filter(
+            (stock: any) => stock.exchange.trim() === "NASDAQ"
+        );
+        return nasdaqStocks;
     }
 
-    const query = useQuery({ queryKey: ['nasdaq'], queryFn: fetchStocks })
+    const { data, error, isLoading } = useQuery<any, Error>({
+        queryKey: ['nasdaq'],
+        queryFn: fetchStocks,
+        staleTime: 1000 * 60 * 60 * 12,
+        gcTime: 1000 * 60 * 60 * 24,
+        refetchOnWindowFocus: false,
 
-    useEffect(() => {
-        fetchStocks();
-    }, [])
+    })
 
-    return { loading, data, error, query };
+    return { loading: isLoading, data, error };
 }
-
