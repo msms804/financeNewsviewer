c9a738cd8044af6f2710362ca2fba2d39d6e035a
msms804 <104570214+msms804@users.noreply.github.com>
2024-09-04 23:16:02 +0900

    로직 수정

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { NewsItem } from './NewsItem'
 import axios from 'axios';
 import usePromise from '../lib/usePromise';
+import { useInView } from 'react-intersection-observer';
+import { useInfiniteQuery } from '@tanstack/react-query';
 
+//import { fetchTranslation } from '../lib/fetchTranslation';
 interface IArticle {
     source: { id: string | null; name: string };
     author: string | null;
@@ -37,55 +40,120 @@ export interface INews {
 interface ICategoryProps {
     category: string;
 }
+interface INewsPage {
+    articles: INews[];
+    isLastPage: boolean;
+    nextPage?: number;
+}
+const fetchTranslation = async ({ pageParam = 0 }: { pageParam?: number }) => {
+    const apiKey = import.meta.env.VITE_NYTIMES_API_KEY;
+    const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=stock&sort=newest&fq=news_desk:("Business")&page=${pageParam}&api-key=${apiKey}`;
+
+    const response = await axios.get(url);
+    const articles = response.data.response.docs;
+
+    console.log("씨이발", articles);
+    return articles;
+
+
+};
 export const NewsList: React.FC<ICategoryProps> = ({ category }) => {
     //const [articles, setArticles] = useState<IArticle[] | null>(null);
+    const { ref, inView } = useInView();
+
+    const {
+        data,
+        error,
+        fetchNextPage,
+        hasNextPage,
+        isFetching,
+        isFetchingNextPage,
+        status
+    } = useInfiniteQuery({
+        queryKey: ['news', category],
+        queryFn: fetchTranslation,
+        initialPageParam: 0,
+        getNextPageParam: (lastPage, pages) => {
+            return lastPage.length ? pages.length : undefined;
+        },
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+    });
+
     const [isLoading, setLoading] = useState(false);
 
     const [news, setNews] = useState<INews[] | null>(null);
-    const [loading, response, error] = usePromise(() => {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`)
-    }, category)
-    // const fetchData = async () => {
+    // const [loading, response, error] = usePromise(() => {
+    //     const query = category === 'all' ? '' : `&category=${category}`;
+    //     return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`)
+    // }, category)
+
+    // useEffect(() => {
+    //     if (inView && hasNextPage) {
+    //         console.log("다음페이지!")
+    //         fetchNextPage();
+
+    //         console.log("페이지", data);
+    //     }
+    // }, [inView, hasNextPage, fetchNextPage])
+    // const fetchNews = async () => {
     //     setLoading(true);
+    //     const apiKey = import.meta.env.VITE_NYTIMES_API_KEY;
+    //     const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=stock&sort=newest&fq=news_desk:("Business")&page=2&api-key=${apiKey}`;
+
     //     try {
-    //         const query = category === 'all' ? '' : `&category=${category}`
-    //         const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=`)
-    //         setArticles(response.data.articles);
-    //         console.log(response.data);
+    //         const response = await axios.get(url);
+    //         //console.log(response.data);//이거 없으니까 에러남;;
+    //         setNews(response.data.response.docs);
     //     } catch (error) {
-    //         console.log(error);
+    //         console.error(error);
     //     }
     //     setLoading(false);
     // }
-    const fetchNews = async () => {
-        setLoading(true);
-        const apiKey = import.meta.env.VITE_NYTIMES_API_KEY;
-        const url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=stock&fq=news_desk:("Business")&api-key=${apiKey}`;
-
-        try {
-            const response = await axios.get(url);
-            //console.log(response.data);//이거 없으니까 에러남;;
-            setNews(response.data.response.docs);
-        } catch (error) {
-            console.error(error);
-        }
-        setLoading(false);
-    }
     useEffect(() => {
         //fetchData();
-        fetchNews();
+        // fetchNews();
     }, [category])
-    if (loading) return <div>대기 중...</div>
-    if (error) return <div>에러 발생!</div>
-    if (!response) return null;
-    if (isLoading) return <div>nyt 로딩중 ...</div>
-    const { articles } = response.data;
+    // if (loading) return <div>대기 중...</div>
+    if (error) return <div>에러 발생! {error.message}</div>
+    // if (!response) return null;
+    //if (isFetching) return <div>nyt 로딩중 ...</div>
+    // const { articles } = response.data;
 
     return (
         <div>
             {/* {articles.map((article: IArticle) => <NewsItem key={article.url} article={article} />)} */}
-            {news && news.map((item: INews) => <NewsItem key={item._id} news={item} />)}
+            {/* {news && news.map((item: INews) => <NewsItem key={item._id} news={item} />)} */}
+            {/* <div ref={ref} className='bg-blue-200'>옵저버</div> */}
+            <div>
+                {data?.pages.map((page, pageIndex) => (
+                    <React.Fragment key={pageIndex}>
+                        {page.map((article: any) => (
+                            <div key={article._id}>
+                                <h2>{article.headline.main}</h2>
+                                <p>{article.snippet}</p>
+                            </div>
+                        ))}
+                    </React.Fragment>
+                ))}
+
+                <div>
+                    {isFetchingNextPage ? (
+                        <p>Loading more...</p>
+                    ) : (
+                        <button
+                            className='cursor-pointer'
+                            onClick={() => { fetchNextPage() }}
+                            disabled={!hasNextPage || isFetchingNextPage}
+                        >
+                            Load More
+                        </button>
+                    )}
+                </div>
+                <div>
+                    <div ref={ref}>옵저버</div>
+                </div>
+            </div>
         </div>
     )
 }
