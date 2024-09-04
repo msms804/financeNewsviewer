eb869462634f697c2b6592dad011c82a27ee3c45
msms804 <104570214+msms804@users.noreply.github.com>
2024-09-05 02:01:23 +0900

    인수명 변경

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -3,31 +3,31 @@ import dayjs from 'dayjs'
 import { useCallGPT } from '../lib/useCallGPT';
 import 'dayjs/locale/ko';
 
-export const NewsItem = ({ news }: any) => {//article로 바꾸고싶음
+export const NewsItem = ({ article }: any) => {//article로 바꾸고싶음
     const [date, setDate] = useState('');
     const [time, setTime] = useState('');
     const [newHeadline, setNewHeadline] = useState('');
     const [newSnippet, setNewSnippet] = useState('');
 
     const convertToKorean = async () => {
-        const headline = await useCallGPT(news.headline.main)
-        const snippet = await useCallGPT(news.snippet)
+        const headline = await useCallGPT(article.headline.main)
+        const snippet = await useCallGPT(article.snippet)
         setNewHeadline(headline);
         setNewSnippet(snippet);
     }
 
     useEffect(() => {
         dayjs.locale("ko");
-        const datetime = dayjs(news.pub_date);
+        const datetime = dayjs(article.pub_date);
         const datePart = datetime.format('YYYY년 MM월 DD일 dddd');
         const timePart = datetime.format('HH:mm');
         setDate(datePart);
         setTime(timePart);
 
-        if (news) {
-            convertToKorean();
+        if (article) {
+            //convertToKorean();
         }
-    }, [news])
+    }, [article])
 
     return (
         <div className='flex flex-col mb-6'>
@@ -45,15 +45,15 @@ export const NewsItem = ({ news }: any) => {//article로 바꾸고싶음
                         <path fillRule="evenodd" d="M8.914 6.025a.75.75 0 0 1 1.06 0 3.5 3.5 0 0 1 0 4.95l-2 2a3.5 3.5 0 0 1-5.396-4.402.75.75 0 0 1 1.251.827 2 2 0 0 0 3.085 2.514l2-2a2 2 0 0 0 0-2.828.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
                         <path fillRule="evenodd" d="M7.086 9.975a.75.75 0 0 1-1.06 0 3.5 3.5 0 0 1 0-4.95l2-2a3.5 3.5 0 0 1 5.396 4.402.75.75 0 0 1-1.251-.827 2 2 0 0 0-3.085-2.514l-2 2a2 2 0 0 0 0 2.828.75.75 0 0 1 0 1.06Z" clipRule="evenodd" />
                     </svg>
-                    <div>자세히 보기</div>
+                    <div>본문</div>
                 </div>
             </div>
             <div className='flex flex-row'>
                 <div className=''>
-                    {/* <div className='font-semibold text-sm mb-1'>{news.headline.main}</div> */}
-                    <div className='font-semibold text-sm mb-1'>{newHeadline}</div>
-                    {/* <div className='text-xs font-light'>{news.snippet}</div> */}
-                    <div className='text-xs font-light'>{newSnippet}</div>
+                    <div className='font-semibold text-sm mb-1'>{article.headline.main}</div>
+                    {/* <div className='font-semibold text-sm mb-1'>{newHeadline}</div> */}
+                    <div className='text-xs font-light'>{article.snippet}</div>
+                    {/* <div className='text-xs font-light'>{newSnippet}</div> */}
                 </div>
 
             </div>
