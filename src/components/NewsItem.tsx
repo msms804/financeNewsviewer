bf7b96337af330e67f7d0eea961a041fb1b6a20e
msms804 <104570214+msms804@users.noreply.github.com>
2024-09-05 02:59:59 +0900

    ui 변경

--- src/components/NewsItem.tsx
+++ src/components/NewsItem.tsx
@@ -31,23 +31,15 @@ export const NewsItem = ({ article }: any) => {//article로 바꾸고싶음
 
     return (
         <div className='flex flex-col mb-6'>
-            <div className='flex justify-between'>
 
-                <div className='flex flex-row items-center gap-2 mb-1'>
-                    <span className='flex text-xs bg-slate-200 rounded-md p-1'>
-                        {time}
-                    </span>
-                    <div className='text-xs text-gray-500'>{date}</div>
+            <div className='flex flex-row items-center gap-2 mb-1'>
+                <span className='flex text-xs bg-slate-200 rounded-md p-1'>
+                    {time}
+                </span>
+                <div className='text-xs text-gray-500'>{date}</div>
 
-                </div>
-                <div className='flex flex-row items-center text-xs text-blue-600'>
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
-                        <path fillRule="evenodd" d="M8.914 6.025a.75.75 0 0 1 1.06 0 3.5 3.5 0 0 1 0 4.95l-2 2a3.5 3.5 0 0 1-5.396-4.402.75.75 0 0 1 1.251.827 2 2 0 0 0 3.085 2.514l2-2a2 2 0 0 0 0-2.828.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
-                        <path fillRule="evenodd" d="M7.086 9.975a.75.75 0 0 1-1.06 0 3.5 3.5 0 0 1 0-4.95l2-2a3.5 3.5 0 0 1 5.396 4.402.75.75 0 0 1-1.251-.827 2 2 0 0 0-3.085-2.514l-2 2a2 2 0 0 0 0 2.828.75.75 0 0 1 0 1.06Z" clipRule="evenodd" />
-                    </svg>
-                    <div>본문</div>
-                </div>
             </div>
+
             <div className='flex flex-row'>
                 <div className=''>
                     <div className='font-semibold text-sm mb-1'>{article.headline.main}</div>
