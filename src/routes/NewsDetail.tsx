a675fbe41ed70bd1db8de4e50d2c788a77b05dfc
msms804 <104570214+msms804@users.noreply.github.com>
2024-09-21 23:30:09 +0900

    .

--- src/routes/NewsDetail.tsx
+++ src/routes/NewsDetail.tsx
@@ -1,8 +1,11 @@
 import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-//이건 토스 따라할거임 ㅋㅋ
-export const NewsDetail = () => {
+import { useLocation, useParams } from 'react-router-dom'
+
+const NewsDetail = () => {
     const { newsId } = useParams();
+    const location = useLocation();
+    const { state } = location;
+    const { abstract, source, relatedStock, headline } = state || {};
 
     return (
         <div>
@@ -18,11 +21,20 @@ export const NewsDetail = () => {
                 </div>
             </div>
             <h1 className='flex flex-col justify-center mb-4 gap-2'>
-                <span className='text-lg font-semibold'>애플은 인공지능(A.I.)에 대해 다르게 생각할 수 있을까?</span>
-                <span className='text-xs text-gray-600'>2024년 08월 17일 09:24 ・ NYT</span>
+                <span className='text-lg font-semibold'>
+                    {headline.main}
+                </span>
+                <span className='text-xs text-gray-600'>2024년 08월 17일 09:24 ・ {source}</span>
             </h1>
-            <div className='flex items-center mb-4'>
-                <div className='border border-gray-400 rounded-full py-1 px-2 text-xs text-gray-500 '>AAPL</div>
+            <div className='flex flex-row items-center gap-1 mb-4'>
+                {relatedStock.map((stock: any) => (
+                    <div
+                        key={stock}
+                        className='border border-gray-400 rounded-full py-1 px-2 text-xs text-gray-500 '
+                    >
+                        {stock}
+                    </div>))}
+
             </div>
             {/* <p>News ID: {newsId}</p> */}
             <div className='  bg-gray-100 rounded-md p-2 mb-4'>
@@ -31,13 +43,15 @@ export const NewsDetail = () => {
             </div>
 
             <div>
-                <div className='text-sm'>
+                <div className='flex flex-col gap-2 text-sm'>
                     <div className='font-semibold'>요약</div>
                     <div>
-                        메타, 엔비디아, 애플을 포함한 7개의 기술 주식이 지난 12개월 동안 S&P의 상승폭의 약 90%를 차지했습니다.
+                        {/* 메타, 엔비디아, 애플을 포함한 7개의 기술 주식이 지난 12개월 동안 S&P의 상승폭의 약 90%를 차지했습니다. */}
+                        {abstract}
                     </div>
                 </div>
             </div>
         </div>
     )
 }
+export default NewsDetail;
