0b58044d8b80eae57dfdf94d4a0a3bc028c9dbcd
msms804 <104570214+msms804@users.noreply.github.com>
2024-11-03 03:05:08 +0900

    테스트 컴포넌트 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useCallback, useEffect, useState, lazy, Suspense } from 'react'
+import { useEffect, useState, lazy, Suspense } from 'react'
 import './App.css'
-import axios from 'axios';
-import { createBrowserRouter, RouterProvider, useRoutes } from 'react-router-dom';
-import { NewsList } from './components/NewsList';
-import { Navbar } from './layouts/Navbar';
-import { Categories } from './components/Categories';
-import { BottomNavbar } from './layouts/BottomNavbar';
-import { Route, Routes } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import Home from './routes/Home';
 import { LoadingScreen } from './components/LoadingScreen';
 import Layout from './layouts/Layout';
 import { auth } from './firebase';
 import ProtectedRoute from './components/ProtectedRoute';
-import { ForTest } from './components/ForTest';
 
 //lazy로 필요한 컴포넌트들을 비동기로 로드
 const My = lazy(() => import('./routes/My'));
@@ -74,10 +67,6 @@ function App() {
       path: '/create-account',
       element: <CreateAccount />
     },
-    {
-      path: '/nyt-test',
-      element: <ForTest />
-    }
   ];
   const routing = useRoutes(routes)
   return (
