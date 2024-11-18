f570b3fa270196c8e968691a5678dcdd6c53d8fa
msms804 <104570214+msms804@users.noreply.github.com>
2024-11-19 00:32:25 +0900

    react-spinners로 변경

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,18 +1,18 @@
 import { CSSProperties, useState } from "react";
-import { PuffLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners";
 
 const override: CSSProperties = {
     display: "block",
     margin: "0 auto",
     borderColor: "#0bb1b1",
 };
-export const LoadingScreen = () => {
+export const Loading = () => {
     const [loading] = useState(true);
     const [color] = useState("#0bb1b1");
     return (
         <div>
-            {/* <img src="/LoadingScreen.gif" alt="LoadingScreen" /> */}
-            <PuffLoader
+            {/* <img src="/Loading5.gif" alt="Loading" /> */}
+            <ClipLoader
                 color={color}
                 loading={loading}
                 cssOverride={override}
